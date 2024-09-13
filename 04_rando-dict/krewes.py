059994f3e45f6ff2ebe54d3c2c5c2e1fc83208d9
Michelle <jzhu50@stuy.edu>
2024-09-13 10:58:37 -0400

    k04

---
+++ 04_rando-dict/krewes.py
@@ -0,0 +1,13 @@
+Michelle Zhu
+Boas
+SoftDev
+K04 -- random and selection
+2024-09-13
+time spent: 0.5
+
+import random
+
+krewes = {3:[1,2,3],42:[1,2,3],888:[1,2,3]}
+
+selected_list = random.choice(krewes)
+print(selected_list)
