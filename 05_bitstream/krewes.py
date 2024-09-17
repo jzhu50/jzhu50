3b96db9317e0c74a19853bd52764cb36e9f95307
Michelle <jzhu50@stuy.edu>
2024-09-17 11:01:35 -0400

    update

---
+++ 05_bitstream/krewes.py
@@ -0,0 +1,18 @@
+'''
+Michelle Zhu
+Boas
+SoftDev
+K05 -- bitstream
+2024-09-17
+time spent: 0.5
+'''
+
+'''name = open("krewes.txt", "r")
+print(name.read())
+'''
+
+krewes = '04$$$jzhu50$$$mishy@@@05$$$qzhou50$$$zhou'
+
+print(krewes.split('$$$', 4))
+
+
