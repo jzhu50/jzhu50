5a4e21c89a54d9c0f5330eff4fd08739a75d3632
Michelle <jzhu50@stuy.edu>
2024-09-18 10:47:48 -0400

    update

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -9,10 +9,19 @@ time spent: 0.5
 
 def makeRoster (filename):
     data = open(filename).read()
-    tuples = data.rsplit("@@@")[;-1]
+    tuples = data.rsplit("@@@")[:-1]
     names = []
     information = []
+    
     for i in range(len(tuples)):
+        tuples[i] = tuples[i].rsplit("$$$")
+        names.append(tuples[i][1])
+        information.append([tuples[i][0],tuples[i][2]])
+        
+    
+    print(roster)
+
+makeRoster(krewes)
         
 
 
