2476d28e7879dfae049ed9b78418b3b2c5593bd5
Michelle <jzhu50@stuy.edu>
2024-09-18 20:23:27 -0400

    update

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -7,21 +7,49 @@ K05 -- bitstream
 time spent: 0.5
 '''
 
-def makeRoster (filename):
-    data = open(filename).read()
-    tuples = data.rsplit("@@@")[:-1]
-    names = []
-    information = []
+import random
+
+data = open("krewes").read()
+print(data)
+
+'''
+we want to make names the keys for the dictionary
+
+1. split the string by person at @@@
+2. split the fields at $$$
+3.
+'''
+
+def parse_people_data(string):
+    records = string.split('@@@') # step 1
+    people_info = {} # dictionary {} to store the data
     
-    for i in range(len(tuples)):
-        tuples[i] = tuples[i].rsplit("$$$")
-        names.append(tuples[i][1])
-        information.append([tuples[i][0],tuples[i][2]])
+    for record in records:
+        period, name, ducky = record.split('$$$') # stpe 2
+        # store info using names as keys
+        # !!! dictionary_name[key_name] = dictionary items
+        people_info[name] = period, ducky
+    return people_info
         
+parse_people_data(data)
+
+def get_info(name, people_info): # get info for selected person
+    if name in people_info: # check if name is in dictionary
+        period, ducky = people_info[name] # get period and ducky name for selected person
+        print('Name: ' + name + '\n' + 'Period: ' + period + '\n' + 'Ducky: ' + ducky)
+        
+get_info('michelle', parse_people_data(data))
+
+
+def random_get_info(people_info):
+    # dict.keys() returns the keys of a dictionary
+    # random.choice() only works with lists
+    # convertion from dict to list needed
+    # list(dict) converts dict to list
+    random_name = random.choice(list(people_info.keys()))
     
-    print(roster)
-
-makeRoster(krewes)
-        
-
-
+    if random_name in people_info:
+        period, ducky = people_info[random_name] # get period and ducky name for selected person
+        print('Name: ' + random_name + '\n' + 'Period: ' + period + '\n' + 'Ducky: ' + ducky)
+    
+random_get_info(parse_people_data(data))
