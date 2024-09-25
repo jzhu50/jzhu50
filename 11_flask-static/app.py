583fb15167ab0ac263955ebfbdd423a012c684d3
Michelle <jzhu50@stuy.edu>
2024-09-25 15:36:29 -0400

    update

--- 11_flask-static/app.py
+++ 11_flask-static/app.py
@@ -8,13 +8,12 @@ import random
 from flask import Flask
 app = Flask(__name__)
 
-'''
+
 @app.route("/")
 def hello_world():
     print("the __name__ of this module is... ")
     print(__name__)
     return "No hablo queso!"
-'''
 
 @app.route("/static/foo.html")
 def h():
