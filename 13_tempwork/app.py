73a64f3bc667a61c0cc4881f3d20aa95be375487
Michelle <jzhu50@stuy.edu>
2024-09-30 15:50:05 -0400

    update

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -10,10 +10,43 @@ time sepnt:
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/wdywtbwygp")
+def occupations_dict():
+    occupations_str = open('occupations.csv').read()
+    records = occupations_str.split('\n')
+    records.pop(0)
+    records.pop(-1) 
+    records.pop(-1)
 
+    occupations_info = {}
+    
+    for record in records:
+        occupation, percentage = record.rsplit(',', 1)
+        occupation = occupation.replace('"',' ') # get rid of quotation marks
+        occupations_info[occupation] = float(percentage)
+    return occupations_info
+
+def table():
+    occupations = occupations_dict()
+    head = "<table><tr><th>Job Class</th><th>Percentage</th><tr>"
+    body = ""
+    for i in occupations:
+        body += "<tr><td>" + i + "</td><td>" + str(occupations[i]) + "</td></tr>"
+    output = head + body + "</table>"
+    return output
+
+# select occupations 100 times based on the percentage weights
+def random_selection(occupations_info, num):
+    occupations = list(occupations_info.keys())
+    percentages = list(occupations_info.values())
+    return random.choices(occupations, weights=percentages, k=num)
+
+@app.route("/")
+def main():
+    return "head to / wdywtbwygp"
+
+@app.route("/wdywtbwygp")
 def tmplt():
-    return render_template("tablified.html", title = "foo")
+    return render_template("tablified.html", title = "13_combine", table = table(), occupation = random_selection(occupation_dict(),1))
 
 if __name__ == "__main__":
     app.debug = True
