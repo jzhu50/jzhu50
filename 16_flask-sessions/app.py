862872a97aab98bf92709ada42430efe13bb1c82
Michelle <jzhu50@stuy.edu>
2024-10-11 11:01:31 -0400

    update

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -7,6 +7,8 @@ K16 -- flask_sessions
 time spent: 0.5
 '''
 
+#https://www.youtube.com/watch?v=dowTXbUi7Jo
+
 # import conventions:
 # list most general first (standard python library)
 # ...then pip installs (eg Flask)
@@ -31,14 +33,25 @@ def disp_loginpage():
 @app.route("/auth", methods=['GET', 'POST'])
 def authenticate():
     username = request.form['username'] if request.method == 'POST' else request.args.get('username')
-    app.set_cookie('MyCookie', user_id)
-    cookie = request.cookies.get('MyCookie')
+    response.set_cookie('MyCookie', user_id)
     return render_template( 'response.html', name = username)
 
 @app.route("/out")
 def disp_logoutpage():
     request.cookies.pop('MyCookie')
     return render_template( 'logout.html')
+
+@app.route("/set_cookie")
+def set_cookie():
+    response = make_response("cookie set")
+    response.set_cookie("cookie_name", "cookie_value")
+    return response
+    
+@app.route("/delete_cookie")
+def delete_cookie():
+    response = make_response("cookie deleted")
+    response.delete_cookie("cookie_name")
+    return reponse
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
