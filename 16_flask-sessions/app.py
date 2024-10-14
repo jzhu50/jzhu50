4aa5933742131f35ac1384213957f0cade0ebc8b
Michelle <jzhu50@stuy.edu>
2024-10-14 15:23:28 -0400

    update

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -4,55 +4,47 @@ Boas
 SoftDev
 K16 -- flask_sessions
 2024-10-11
-time spent: 0.5
+time spent: 1.5
 '''
 
-#https://www.youtube.com/watch?v=dowTXbUi7Jo
-# use session for core assignment
-
-# import conventions:
-# list most general first (standard python library)
-# ...then pip installs (eg Flask)
-# ...then your own home-rolled modules/packages (today's test module)
-
-from flask import Flask             #facilitate flask webserving
-from flask import render_template   #facilitate jinja templating
-from flask import request           #facilitate form submission
+from flask import Flask
+from flask import render_template   
+from flask import request
 from flask import session
+from flask import redirect
+from flask import url_for
 
-#the conventional way:
-#from flask import Flask, render_template, request
+app = Flask(__name__)
+app.secret_key = 'haha'
 
-app = Flask(__name__)    #create Flask object
-# app.secret_key = "haha"
+@app.route('/')
+def index():
+    if 'username' in session:
+        return redirect(url_for('response'))
+    return redirect(url_for('login'))
 
-@app.route("/")
-def disp_loginpage():
-    user_id = request.cookies.get('MyCookie')
-    return render_template( 'login.html' )
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        if username:  # If a username is provided, log in the user
+            session['username'] = username
+            return redirect(url_for('response'))
+        else:
+            return 'Please enter a username!'
+    return render_template('login.html')
 
-@app.route("/auth", methods=['GET', 'POST'])
-def authenticate():
-    username = request.form['username'] if request.method == 'POST' else request.args.get('username')
-    response.set_cookie('MyCookie', user_id)
-    return render_template( 'response.html', name = username)
+# Allow only GET requests to the response page
+@app.route('/response', methods=['GET'])
+def response():
+    if 'username' in session:
+        return render_template('response.html', username=session['username'])
+    return redirect(url_for('login'))
 
-@app.route("/out")
-def disp_logoutpage():
-    request.cookies.pop('MyCookie')
-    return render_template( 'logout.html')
-
-@app.route("/set_cookie")
-def set_cookie():
-    response = make_response("cookie set")
-    response.set_cookie("cookie_name", "cookie_value")
-    return response
-    
-@app.route("/delete_cookie")
-def delete_cookie():
-    response = make_response("cookie deleted")
-    response.delete_cookie("cookie_name")
-    return reponse
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
