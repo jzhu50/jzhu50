4c090b887faf5120e3e0c03fde0f12f14875f31d
Michelle <jzhu50@stuy.edu>
2024-10-19 18:18:46 -0400

    update

--- 19_csv2db/build_db.py
+++ 19_csv2db/build_db.py
@@ -1,28 +1,30 @@
-#Clyde "Thluffy" Sinclair
+#Boas: Michelle Zhu, Ryan Zhou, Linda Zheng
 #SoftDev
 #skeleton/stub :: SQLITE3 BASICS
-#Oct 2024
+#10/20/2024
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 
 
-DB_FILE="discobandit.db"
+DB_FILE="00.db"
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
 #==========================================================
 
+#c.execute("CREATE TABLE roster (name TEXT, age INTERGER, id INTEGER)")
 
-"""
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-< < < INSERT YOUR TEAM'S DB-POPULATING CODE HERE > > >
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-"""
-
-command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
+with open('students.csv', mode='r', newline='') as file:
+    csv_reader = csv.DictReader(file) #reads csv file as an ordered dictionary
+    for row in csv_reader:
+        #print(row)
+        name = (row['name'])
+        age = (row['age'])
+        id = (row['id'])
+        command = "INSERT INTO roster (name, age, id) VALUES (?, ?, ?)" # test SQL stmt in sqlite3 shell, save as string
+        c.execute(command, (name, age, id))    # run SQL statement
 
 #==========================================================
 
